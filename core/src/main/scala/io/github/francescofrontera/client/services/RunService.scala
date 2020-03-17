0dc0af268272411a3d337be1f9279606cf47826c
francescofrontera <fra.frontera01@gmail.com>
2020-03-17 12:49:18 +0100

    Mirror code refactoring

--- core/src/main/scala/io/github/francescofrontera/client/services/RunService.scala
+++ core/src/main/scala/io/github/francescofrontera/client/services/RunService.scala
@@ -15,18 +15,16 @@ object RunService {
   private[this] val RunURL: Seq[String] = "runs" +: Nil
   val live: ZLayer[InternalClient, Nothing, RunService] = ZLayer.fromFunction(
     ic =>
-      new Service {
-        def getById(runId: String): Task[Run] =
-          for {
-            url <- ic.get.url
-            call <- ic.get.genericGet[Run](
-              URLUtils.makeURL(
-                basePath = url,
-                pathParameters = RunURL ++ Seq("get"),
-                queryParameters = Map("run_id" -> runId)
-              )
+      (runId: String) =>
+        for {
+          url <- ic.get.url
+          call <- ic.get.genericGet[Run](
+            URLUtils.makeURL(
+              basePath = url,
+              pathParameters = RunURL ++ Seq("get"),
+              queryParameters = Map("run_id" -> runId)
             )
-          } yield call
-    }
+          )
+        } yield call
   )
 }
