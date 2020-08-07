9c26e3b4f146d22d1614fcdf923866988423bd55
francescofrontera <francesco.frontera@igenius.ai>
2020-08-07 15:12:40 +0200

    Upgaraded dependencies

--- core/src/main/scala/io/github/francescofrontera/utils/URLUtils.scala
+++ core/src/main/scala/io/github/francescofrontera/utils/URLUtils.scala
@@ -5,14 +5,12 @@ import java.net.URI
 import sttp.model.Uri
 
 object URLUtils {
-  private[this] def composedURL(paths: Seq[String]): String => String =
-    basePath => paths.foldLeft(basePath)((acc, path) => Seq(acc, path) mkString "/")
-
   def makeURL(pathParameters: Seq[String], basePath: String, queryParameters: Map[String, String] = Map.empty): Uri = {
-    val normalizeQueryParameters = queryParameters map {
-      case (key, value) => s"$key=$value"
-    } mkString ","
+    val normalizeQueryParameters = queryParameters map { case (key, value) => s"$key=$value" } mkString ","
 
     Uri(new URI(s"${composedURL(pathParameters)(basePath)}?$normalizeQueryParameters"))
   }
+
+  private[this] def composedURL(paths: Seq[String]): String => String =
+    basePath => paths.foldLeft(basePath)((acc, path) => Seq(acc, path) mkString "/")
 }
